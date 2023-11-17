057ccce72cb8bfb54d92f9f1681bca7eb2ea5013
netbrum <netbrum@tutanota.com>
2023-11-17 23:30:31 +0100

    feat: add editor core loop

--- src/editor.rs
+++ src/editor.rs
@@ -3,11 +3,13 @@ mod mode;
 use mode::Mode;
 
 use super::buffer::{AlternateBuffer, IntoAlternateBuffer};
+use super::event::Key;
+use super::input::EventIterator;
 use super::raw::{IntoRawMode, RawTerminal};
 
 use std::{
     env::Args,
-    io::{self, Result, Stdout},
+    io::{self, Result, Stdout, Write},
 };
 
 pub struct Editor {
@@ -23,5 +25,35 @@ impl Editor {
             mode: Mode::Normal,
             out,
         })
+    }
+
+    pub fn run(&mut self) {
+        let mut stdin = io::stdin().lock().keys();
+
+        loop {
+            if self.mode == Mode::Exit {
+                break;
+            }
+
+            if let Some(Ok(key)) = stdin.next() {
+                self.handle_key(key);
+            }
+        }
+    }
+
+    fn print(&mut self, text: &str) -> Result<()> {
+        self.out.write_all(text.as_bytes())?;
+        self.out.flush()?;
+
+        Ok(())
+    }
+
+    fn handle_key(&mut self, key: Key) {
+        match key {
+            Key::Ctrl('c') => self.mode = Mode::Exit,
+            k => {
+                self.print(&format!("{:?}\r\n", k)).unwrap();
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,14 @@
-fn main() {}
+use std::env;
+
+use alice::editor::Editor;
+
+fn main() {
+    let editor = Editor::new(env::args());
+
+    match editor {
+        Ok(mut e) => e.run(),
+        Err(err) => {
+            eprintln!("{}", err);
+        }
+    }
+}
