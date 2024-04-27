42cb977aaf621ccbff46583697a93ea258b318f9
netbrum <netbrum@tutanota.com>
2024-04-27 22:30:29 +0200

    chore: bump tree-sitter dependencies

--- src/highlight/rust.rs
+++ src/highlight/rust.rs
@@ -6,8 +6,13 @@ impl HighlightConfig for Rust {
     fn config() -> Result<HighlightConfiguration, QueryError> {
         let language = tree_sitter_rust::language();
 
-        let mut config =
-            HighlightConfiguration::new(language, tree_sitter_rust::HIGHLIGHT_QUERY, "", "")?;
+        let mut config = HighlightConfiguration::new(
+            language,
+            "rust",
+            tree_sitter_rust::HIGHLIGHTS_QUERY,
+            "",
+            "",
+        )?;
 
         config.configure(&DEFAULT_HIGHLIGHT_NAMES);
 
